b0bd5b3a66cb24206f502e1f4e223c8ea929eff8
CamilaAchuri <camilaachury.4@gmail.com>
2023-05-16 14:43:34 -0500

    create list with general updates specifications

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+#usethis::use_data(DATASET, overwrite = TRUE)
+
+
+input_list <- jsonlite::read_json("data-raw/data/inputs.json")
+input_list$selector
+names(input_list$inputs)
